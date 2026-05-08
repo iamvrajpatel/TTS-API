8c62dd148be65df662bda880fb271ef2174c9105
Vraj Patel <vraj.patel@hubblehox.com>
2026-05-08 17:18:30 +0530

    Update server port in main.py from 8000 to 5000

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ app = create_app(service=tts_service, catalog=catalog)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
